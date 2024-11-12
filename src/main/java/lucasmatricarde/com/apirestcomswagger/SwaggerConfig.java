0ff0ccfb9ae8538297b6f238f9cb1c78ad23f276
Lucas Matricarde <lucas.eduardo@wises.com.br>
2024-11-12 20:59:33 -0300

    Faltou as anotations

--- src/main/java/lucasmatricarde/com/apirestcomswagger/SwaggerConfig.java
+++ src/main/java/lucasmatricarde/com/apirestcomswagger/SwaggerConfig.java
@@ -1,16 +1,20 @@
 package lucasmatricarde.com.apirestcomswagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
     private Contact contato() {
         return new Contact(
